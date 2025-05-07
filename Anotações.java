526da3ae4a5ded48f5abc6f4f9ecd6cfe9701afb
João Pedro <jpappelt10@gmail.com>
2025-05-07 20:13:15 -0300

    Update Anotações.java

--- Anotações.java
+++ Anotações.java
@@ -1,1 +1,12 @@
-
+Java NetBeans
+// SEMPRE USAR PONTO E VIRGULA NO FINAL DE CADA LINHA;
+  
+// caso queira rodar o codigo aperta alt f6;
+  
+// se voce segurar ctrl em cima do metodo ele mostra informações e caso clique ele irá ate esse metodo.
+  
+// o metodo constructor, em java, deve ser delcarado como public.
+// NÃO DEVE IR RETORNO(nem void).
+// Deve ter o mesmo nome EXATO da classe.
+// nao existe a palavra constructor em java.
+  
