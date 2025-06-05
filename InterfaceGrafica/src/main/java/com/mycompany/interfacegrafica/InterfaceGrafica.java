fe394eb2ba17b0cd28aa0cf50f373a017d6afcee
João Pedro <jpappelt10@gmail.com>
2025-06-04 21:33:01 -0300

    Add files via upload

---
+++ InterfaceGrafica/src/main/java/com/mycompany/interfacegrafica/InterfaceGrafica.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.interfacegrafica;
+
+/**
+ *
+ * @author JOAOPEDRODESBESELAPP
+ */
+public class InterfaceGrafica {
+
+    public static void main(String[] args) {
+        new TelaINT01().setVisible(true);
+    }
+}
